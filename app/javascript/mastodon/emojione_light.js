a6f74dc66eaaeac2ff7736be54f7c20b27387682
Eugen Rochko <eugen@zeonfederated.com>
2017-09-24 16:44:53 +0200

    Fix outdated emoji codepoints by making emojify() use emoji-mart data

--- app/javascript/mastodon/emojione_light.js
+++ app/javascript/mastodon/emojione_light.js
@@ -1,13 +1,40 @@
 // @preval
 // Force tree shaking on emojione by exposing just a subset of its functionality
 
-const emojione = require('emojione');
+const { emojis }    = require('emoji-mart').emojiIndex;
+const mappedUnicode = {};
+const excluded      = ['®', '©', '™'];
+const skins         = ['1F3FA', '1F3FB', '1F3FC', '1F3FD', '1F3FE', '1F3FF'];
 
-const mappedUnicode = emojione.mapUnicodeToShort();
-const excluded = ['®', '©', '™'];
+const fromCodePoint = codepoint => {
+  let code = typeof codepoint === 'string' ? parseInt(codepoint, 16) : codepoint;
 
-module.exports.unicodeMapping = Object.keys(emojione.jsEscapeMap)
-  .filter(c => !excluded.includes(c))
-  .map(unicodeStr => [unicodeStr, mappedUnicode[emojione.jsEscapeMap[unicodeStr]]])
-  .map(([unicodeStr, shortCode]) => ({ [unicodeStr]: [emojione.emojioneList[shortCode].fname.replace(/^0+/g, ''), shortCode.slice(1, shortCode.length - 1)] }))
-  .reduce((x, y) => Object.assign(x, y), { });
+  if (code < 0x10000) {
+    return String.fromCharCode(code);
+  }
+
+  code -= 0x10000;
+
+  return String.fromCharCode(
+    0xD800 + (code >> 10),
+    0xDC00 + (code & 0x3FF)
+  );
+};
+
+Object.keys(emojis).forEach(shortcode => {
+  const emoji = emojis[shortcode];
+
+  if (excluded.includes(emoji.native)) {
+    return;
+  }
+
+  mappedUnicode[emoji.native] = [emoji.unified, shortcode];
+
+  if (emoji.skin !== null) {
+    skins.forEach((skinCodePoint, skinIndex) => {
+      mappedUnicode[emoji.native + fromCodePoint(skinCodePoint)] = [[emoji.unified, skinCodePoint.toLowerCase()].join('-'), shortcode + `::skin-tone-${skinIndex + 1}`];
+    });
+  }
+});
+
+module.exports.unicodeMapping = mappedUnicode;
